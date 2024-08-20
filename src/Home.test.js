87de009188f2a25a8e93ddc5d05f3a55bc81ea61
EricaMoua <erica.moua568@gmail.com>
2024-08-20 13:39:29 -0400

    fixed module

--- src/Home.test.js
+++ src/Home.test.js
@@ -1,15 +1,35 @@
-// Home.test.js
-import React from 'react';
-import { render } from '@testing-library/react';
-import Home from './pages/Home';
+import { render, screen } from "@testing-library/react";
+import Home from "../src/pages/Home";
+import React from "react";
 
-jest.mock('../public/lrnr-logo.png', () => 'mock-logo.png');
 
-test('renders Home component correctly', () => {
-  const { getByAltText } = render(<Home />);
-
-  // Check that the logo image is rendered
-  const logoImage = getByAltText('lrnr-logo');
-  expect(logoImage).toBeInTheDocument();
-  expect(logoImage).toHaveAttribute('src', 'mock-logo.png');
+test("Always true test", () => {
+  expect(true).toBe.true;
 });
+
+test("check header test", () => {
+  render(<Home/>);
+  const header = screen.getByText(/Your guided path to programming enlightenment/i);
+  expect(header).toBeInTheDocument();
+})
+
+test("check main button test", () => {
+  render(<Home/>);
+  const button = screen.getByText(/Begin Journey/i);
+  expect(button).toBeInTheDocument();
+});
+
+test('checks main logo with correct image and alt text', () => {
+  render(<Home/>);
+  const logo = screen.getByAltText('lrnr-logo');
+  expect(logo).toBeInTheDocument();
+  expect(logo).toHaveAttribute('alt', 'lrnr-logo');
+});
+
+test("check main button test", () => {
+  render(<Home/>);
+  const button = screen.getByText(/Begin Journey/i);
+  expect(button).toHaveClass('btn-large');
+  expect(button).toHaveAttribute('id', 'download-button');
+});
+
